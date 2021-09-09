d22ff7f87dd825a7de7a25772b80a385b76b566b
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-09 20:54:15 +0300

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    no = len(sys.argv) - 1
+    if no == 0:
+        print("0 arguments.")
+    elif no == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(no))
+    for i in range(no):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    
