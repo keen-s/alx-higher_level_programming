92d0991a07d84c2e0b45965e9dff43f2f3ec11c3
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-16 16:18:47 +0300

    New file

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        if i in set_2:
-            set_4 = []
-            set_4.append(i)
-    return set(set_4)
+    return (set_1 & set_2)
