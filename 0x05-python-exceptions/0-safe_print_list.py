426f4605cb7e30a9ac798125ec4e4916eff7e2e1
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-27 21:48:05 +0300

    New file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-    list_count = 0
+    ret = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            list_count += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
     print("")
-    return list_count
+    return (ret)
