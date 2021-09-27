2c6d6b44ada89bab76743425376880a9bd8434a5
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-27 21:00:39 +0300

    New updated files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Writting a function that prints x elements in a list
+
 def safe_print_list(my_list=[], x=0):
     list_count = 0
     for i in range(x):
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    # a function that divides 2 integers and prints the results.
+    try:
+        ans = a / b
+    except (TypeError, ZeroDivisionError):
+        ans = None
+    finally:
+        print("Inside result: {}".format(ans))
+    return ans
