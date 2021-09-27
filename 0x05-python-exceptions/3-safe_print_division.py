9da87331102ee13ab662240584643b1b46d38337
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-09-27 21:56:51 +0300

    New correcton

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def safe_print_division(a, b):
-    # a function that divides 2 integers and prints the results.
+    # a function that divides 2 integers and prints the results
     try:
         ans = a / b
     except (TypeError, ZeroDivisionError):
