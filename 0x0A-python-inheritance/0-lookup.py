5c0946cc7fd0cf6ad5076da66e5ec29460514a1e
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-10-11 21:42:30 +0300

    First update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Author : Spencer """
+
+def lookup(obj):
+    """ A function that returns the list of
+        available attributes and methods of an object """
+
+    return dir(obj)
