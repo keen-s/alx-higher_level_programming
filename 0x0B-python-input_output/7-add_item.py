df63db8190d53b473f2f92c72b4c6e68d8be5a7b
keen-spe <keen_spe@DESKTOP-C7LRBS8.localdomain>
2021-10-12 22:25:56 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Load, add, save
-A script that adds all arguments to a Python list,
-and then save them to a file
+# 9-add_item.py
+"""Add all arguments to a Python list and save them to a file.
 """
 import sys
 
